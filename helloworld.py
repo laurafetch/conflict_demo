6d51a1dc442e87b94479a31793b1a972c05e1031
Laura Fetch <iamlaurafetch@gmail.com>
2024-08-13 14:42:35 -0400

    merge again

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-def hello_world(state):
-    print(f'Hello World {state}!')
+def hello_world(city, state):
+    print(f'Hello World {city}, {state}!')
 
 
 def main():
-    hello_world('New York')
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> 119562d690b4e8eb211ecf5bad552c8676bb2159
+    hello_world('NYC', 'New York')
 
 
 main()
